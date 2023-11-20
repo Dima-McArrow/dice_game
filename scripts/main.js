bf5486a03feb835a9154d9e4f6f236634978a944
Dima-McArrow <dsm.box@gmail.com>
2023-11-20 23:01:15 +0100

    JavaScript initiated | dice roll func

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,23 @@
 console.info('Dice game V0.5')
+
+const dice = () => {
+  return (Math.floor(Math.random() * 6) + 1)
+}
+
+
+const rollButton = document.getElementById('roll__dice')
+
+console.info('button:', rollButton)
+
+rollButton.addEventListener('click', (e) => {
+  e.preventDefault()
+  let diceNum = dice()
+  rollDiceVisual(diceNum)
+  console.info('dice:', diceNum)
+})
+
+const rollDiceVisual = (dice) => {
+  const cont = document.getElementById('dice')
+  let content = `<img src="./images/dice-${dice}.svg" alt="dice" />`
+  cont.innerHTML = content
+}
